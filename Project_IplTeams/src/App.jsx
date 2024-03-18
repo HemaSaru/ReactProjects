f02210e0d31672eb85b35403b84578d63f839a1d
HemaThapa <hemasaruthapa@gmail.com>
2024-03-18 07:41:00 +0530

    added Project-IPLTeams

--- Project_IplTeams/src/App.jsx
+++ Project_IplTeams/src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [selectedTeam, setSelectedTeam] = useState("CSK");
+  const [selectedTeamImg, setSelectedTeamImg] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE5UPWzjhMnFifbfE7M3dD2upyCwoN9g7FUmHLmMv79Q&s");
+
+  const ChangeTeam = (team, link) => {
+    setSelectedTeam(team);
+    setSelectedTeamImg(link);
+  }
+
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul className="flex gap-10 mx-auto bg-black justify-center p-10">
+          <li className="bg-teal-500 text-white py-2  px-6 rounded-md cursor-pointer hover:bg-teal-700" onClick={() => { ChangeTeam("CSK", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQE5UPWzjhMnFifbfE7M3dD2upyCwoN9g7FUmHLmMv79Q&s") }}>CSK</li>
+          
+          <li className="bg-teal-500 text-white py-2 px-6 rounded-md cursor-pointer hover:bg-teal-700" onClick={() => { ChangeTeam("KKR", "https://m.economictimes.com/thumb/msid-99120720,width-1200,height-900,resizemode-4,imgsize-119668/ipl-2023-kkr-team-review-check-kolkata-knight-riders-strengths-weaknesses-and-key-players.jpg") }}>KKR</li>
+          <li className="bg-teal-500 text-white py-2 px-6 rounded-md cursor-pointer hover:bg-teal-700" onClick={() => { ChangeTeam("MI", "https://staticg.sportskeeda.com/editor/2023/12/f556e-17030116826736-1920.jpg?w=840") }}>MI</li>
+          <li className="bg-teal-500 text-white py-2 px-6 rounded-md cursor-pointer hover:bg-teal-700" onClick={() => { ChangeTeam("RCB", "https://www.royalchallengers.com/PRRCB01/public/styles/817x545_landscape/public/2020-03/RCBSquadWeb.jpg?h=920929c4&itok=GK65M_OV") }}>RCB</li>
+        </ul>
+
+        <div className="flex flex-col items-center mt-10 gap-10">
+          <h1 className="font-bold">{selectedTeam}</h1>
+          <div className="h-52 w-52 bg-green-300 rounded-sm">
+            <img src={selectedTeamImg} className="h-full w-full" alt="" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
